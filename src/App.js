604df5fa0ecec7742a8befa2acb715563b0a4d67
adriancobon <49730380+adriancobon@users.noreply.github.com>
2019-12-04 14:24:31 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,9 +21,7 @@ class App extends React.Component {
       todo: ''
     };
   }
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+//  Here be dragons...
   addTodo = e => {
     e.preventDefault();
     const todos = this.state.todos.slice();
